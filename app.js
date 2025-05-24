a57af5167f05d8c6d0b3e9bd82b2a31d9b511516
sarthakshishodia20 <sarthakshishodia2004@gmail.com>
2025-05-24 09:56:07 +0530

    Render Changes

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ io.on('connection',function(uniqueSocket){
 });
 
 // Start the server
-server.listen(3000, function () {
-    console.log("listening on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
